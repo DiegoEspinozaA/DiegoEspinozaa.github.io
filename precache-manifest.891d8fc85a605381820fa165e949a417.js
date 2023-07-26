5daa0cc549d52088e44db2e847d23c368c027d3c
DiegoEspinozaA <91444652+DiegoEspinozaA@users.noreply.github.com>
2023-07-26 08:42:41 -0400

    Subiendo pagina

---
+++ precache-manifest.891d8fc85a605381820fa165e949a417.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03e82217638997956f12145bd98f32f",
+    "url": "/yemo/index.html"
+  },
+  {
+    "revision": "434387ecd2060b1e505d",
+    "url": "/yemo/static/css/main.ce4887f2.chunk.css"
+  },
+  {
+    "revision": "0af606729004dfe0d1fb",
+    "url": "/yemo/static/js/2.e2360fcc.chunk.js"
+  },
+  {
+    "revision": "7574cc173e4511922fab76505fa5d6cc",
+    "url": "/yemo/static/js/2.e2360fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434387ecd2060b1e505d",
+    "url": "/yemo/static/js/main.6422a5f1.chunk.js"
+  },
+  {
+    "revision": "6b17fe356054b5d2cbe4",
+    "url": "/yemo/static/js/runtime-main.46afdf4e.js"
+  },
+  {
+    "revision": "96c06980712a33c574f9933a25e75cf9",
+    "url": "/yemo/static/media/SFNSText-Regular.96c06980.otf"
+  },
+  {
+    "revision": "7a3de74b0bc4869fc8bf5b20500a697f",
+    "url": "/yemo/static/media/cerveza.7a3de74b.webp"
+  },
+  {
+    "revision": "c61f226ceed8e000deb72ce87451da73",
+    "url": "/yemo/static/media/civico.c61f226c.webp"
+  },
+  {
+    "revision": "eeaa609c9fa3c48cdeaaf7bcd5a77422",
+    "url": "/yemo/static/media/fondo.eeaa609c.webp"
+  },
+  {
+    "revision": "cde6e6e9f231296bf20ca9394a4d5dbf",
+    "url": "/yemo/static/media/hamburguesa.cde6e6e9.webp"
+  },
+  {
+    "revision": "fb6693b4dbbf43481749fd4489c8dac9",
+    "url": "/yemo/static/media/textura.fb6693b4.webp"
+  }
+]);
